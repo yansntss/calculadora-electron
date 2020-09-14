6a8f3f063c2b28d5649fcb2e0bcb09cc0caa902b
yansntss <yansantos.dev@gmail.com>
2020-09-14 15:06:55 -0300

    featured image

---
+++ index.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow, Tray } = require("electron");
+
+function createWindow() {
+  const getIcon = new Tray(__dirname + "/src/assets/calculadora.png");
+
+  // Cria uma janela de navegação.
+  const win = new BrowserWindow({
+    width: 300,
+    height: 350,
+    icon: __dirname + "/src/assets/calculadora.png",
+    
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  //remover menu da aplicação
+  
+  
+  // e carrega o arquivo index.html do seu aplicativo.
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
